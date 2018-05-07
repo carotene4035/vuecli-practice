ac7305a5cf71fc664a209f50e185ae0cb7b653c1
carotene512 <karoten512@gmail.com>
2018-05-07 09:30:19 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <myheader></myheader>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+/** 子コンポーネントの読み込み */
+import myheader from './components/myheader'
+
+/** 子コンポーネントを使うことをここで宣言 */
+export default {
+  name: 'App',
+  components: {
+    myheader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
